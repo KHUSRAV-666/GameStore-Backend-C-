7917b6a4d08888cd612e6ee8ad6da48f292f1669
khusrav <asalmamadov.s7@gmail.com>
2026-06-18 16:34:53 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using GameStore.Api.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddValidation();
+
+var app = builder.Build();
+
+app.MapGamesEndpoints();
+app.Run();
